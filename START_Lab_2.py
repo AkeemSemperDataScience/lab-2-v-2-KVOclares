fa46cab0b6a2e2fcf0f2e9b5bd1d4a85590aabb7
Kier <Kiervoclares@gmail.com>
2024-09-15 15:04:21 -0600

    Q1-3

--- START_Lab_2.py
+++ START_Lab_2.py
@@ -2,17 +2,48 @@ def lab2Question1(word):
     # Note - you'll need to change the signature (above) to match the arguments for this lab...
     # Create a function that takes in a string 
     # Return True if that string is a palindrome, False otherwise
+
+    def palindrome_checker(string):
+        return string == string[::-1]
+    
+    return palindrome_checker(word)
     pass
 
 def lab2Question2(number_val):
     # Create a function that takes in a number
     # Return a list of the fibonacci sequence up to that number
+
+    def fibonacci_checker(value):
+        current_fibonacci = []
+        counter = 0
+        number_sequence = 1
+        n1, n2 = 0, 1
+
+        if value <= 0:
+            while counter < number_sequence:
+                if n1 <= value:
+                    current_fibonacci.append(n1)
+                    current_number_sequence = n1 + n2
+                    n1 = n2
+                    n2 = current_number_sequence
+                    number_sequence += 1
+                    counter += 1
+                else:
+                    return current_fibonacci
+        else: 
+            return current_fibonacci
+    
+    return fibonacci_checker(number_val)
     pass
 
 def lab2Question3(str1, str2):
     # Create a function that takes in two strings - str1 and str2
     # Return the number of times str2 appears in str1
     # For example if str1 = "coding is cool" and str2 = "co" then output should be 2.
+    def string_counter(first_string, second_string):
+        return first_string.count(second_string)
+    
+    return string_counter(str1, str2)
     pass
 
 def lab2Question4(list1, list2):
@@ -45,4 +76,4 @@ def isValidPassword(password):
     # - Contains at least one lowercase letter
     # - Contains at least one number
     pass
-
+     
